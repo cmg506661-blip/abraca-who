1fc51ebdb780a0f7971b00967b53be1c61669e04
cmg506661-blip <cmg506661@gmail.com>
2026-06-15 15:16:59 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+
+let players = [];
+let deck = [];
+let turnIndex = 0;
+let gameStarted = false;
+
+const cardNames = {
+    6: '👻고스트(6)', 5: '🧛뱀파이어(5)', 4: '⚔️갑옷기사(4)', 
+    3: '🎩집사(3)', 2: '🍳요리사(2)', 1: '🧹청소부(1)'
+};
+
+function initDeck() {
+    deck = [6, 5,5, 4,4,4, 3,3,3,3, 2,2,2,2,2, 1,1,1,1,1,1];
+    deck.sort(() => Math.random() - 0.5);
+}
+
+function drawCard() {
+    if (deck.length === 0) initDeck();
+    return deck.shift();
+}
+
+function nextTurn() {
+    let aliveCount = players.filter(p => p.hp > 0).length;
+    if (aliveCount <= 1) return; 
+
+    do {
+        turnIndex = (turnIndex + 1) % players.length;
+    } while (players[turnIndex].hp <= 0);
+}
+
+function checkGameState(msg) {
+    let alivePlayers = players.filter(p => p.hp > 0);
+    let stoneWinner = players.find(p => p.stone >= 7);
+    let isOver = false;
+
+    if (stoneWinner) {
+        msg += `\n\n👑 ${stoneWinner.name}님이 신비한 돌 7개를 모아 최종 우승했습니다!`;
+        isOver = true;
+    } else if (alivePlayers.length === 1) {
+        msg += `\n\n👑 최후의 생존자, ${alivePlayers[0].name}님이 최종 우승했습니다!`;
+        isOver = true;
+    } else if (alivePlayers.length === 0) {
+        msg += `\n\n무승부! 모두가 쓰러졌습니다.`;
+        isOver = true;
+    }
+
+    return { msg, isOver };
+}
+
+io.on('connection', (socket) => {
+    if (gameStarted) {
+        socket.emit('gameLog', "이미 게임이 시작되었습니다.");
+        return socket.disconnect();
+    }
+
+    let newPlayer = {
+        id: socket.id,
+        name: `마법사 ${players.length + 1}`,
+        hp: 5, stone: 0, card: 0
+    };
+    players.push(newPlayer);
+    
+    io.emit('lobbyUpdate', players);
+
+    socket.on('startGame', () => {
+        if (players.length < 2) return;
+        gameStarted = true;
+        initDeck();
+        players.forEach(p => p.card = drawCard());
+        io.emit('gameState', { players, turnIndex, log: "🚀 게임이 시작되었습니다! 마법사들의 대결이 펼쳐집니다." });
+    });
+
+    socket.on('actionDuel', (targetId) => {
+        let me = players.find(p => p.id === socket.id);
+        let target = players.find(p => p.id === targetId);
+        
+        let aNum = me.card;
+        let dNum = target.card;
+        let msg = `⚔️ <b>[${me.name}]</b>가 <b>[${target.name}]</b>에게 결투를 신청했습니다!\n공격(${cardNames[aNum]}) VS 수비(${cardNames[dNum]})\n\n`;
+
+        if (aNum === 1 && dNum === 6) {
+            msg += "🎉 🧹청소부가 👻고스트를 잡았습니다!"; target.hp--; me.stone++;
+        } else if (aNum === 6 && dNum === 1) {
+            msg += "💀 👻고스트가 🧹청소부에게 쫓겨납니다."; me.hp--; target.stone++;
+        } else if (aNum === 2 && dNum === 5) {
+            msg += "🎉 🍳요리사가 🧛뱀파이어를 이겼습니다!"; target.hp--; me.stone++;
+        } else if (aNum === 5 && dNum === 2) {
+            msg += "💀 🧛뱀파이어가 🍳요리사에게 당했습니다."; me.hp--; target.stone++;
+        } else if (aNum > dNum) {
+            msg += `🎉 공격 성공! 방어자의 체력이 1 깎입니다.`; target.hp--;
+        } else if (aNum < dNum) {
+            msg += `💀 공격 실패... 역공을 당해 공격자의 체력이 1 깎입니다.`; me.hp--;
+        } else {
+            msg += "🤝 챙챙! 힘이 같아 아무 일도 일어나지 않습니다.";
+        }
+
+        me.card = drawCard(); 
+        
+        let state = checkGameState(msg);
+        if (!state.isOver) nextTurn();
+        io.emit('gameState', { players, turnIndex, log: state.msg, isOver: state.isOver });
+    });
+
+    socket.on('actionGuess', (guessedNumber) => {
+        let me = players.find(p => p.id === socket.id);
+        let pNum = me.card;
+        let msg = `🤔 <b>[${me.name}]</b>의 추리:\n"내 카드는 분명 [${guessedNumber}]일 것이다!"\n\n`;
+
+        if (guessedNumber === pNum) {
+            msg += `🎉 추리 성공! 진짜 카드는 <b>[${cardNames[pNum]}]</b>였습니다!\n✨ <b>[강력한 마법 효과 발동]</b> ✨\n`;
+            switch(pNum) {
+                case 6: msg += "👻고스트: 나머지 전원 즉시 탈락!"; players.forEach(p => { if (p.id !== me.id) p.hp = 0; }); break;
+                case 5: msg += "🧛뱀파이어: 신비한 돌 +2개 획득!"; me.stone += 2; break;
+                case 4: msg += "⚔️갑옷기사: 나머지 전원에게 2 피해!"; players.forEach(p => { if (p.id !== me.id && p.hp > 0) p.hp -= 2; }); break;
+                case 3: msg += "🎩집사: 자신 체력 1 회복, 나머지 전원 1 피해!"; me.hp++; players.forEach(p => { if (p.id !== me.id && p.hp > 0) p.hp -= 1; }); break;
+                case 2: msg += "🍳요리사: 신비한 돌 +1개 획득!"; me.stone++; break;
+                case 1: msg += `🧹청소부: 카드 더미 훔쳐보기를 사용했습니다.`; break; 
+            }
+        } else {
+            msg += `💀 펑! 추리 실패...\n진짜 카드는 <b>[${cardNames[pNum]}]</b>였습니다. 페널티로 체력이 1 깎입니다.`;
+            me.hp--;
+        }
+
+        me.card = drawCard(); 
+
+        let state = checkGameState(msg);
+        if (!state.isOver) nextTurn();
+        io.emit('gameState', { players, turnIndex, log: state.msg, isOver: state.isOver });
+    });
+
+    socket.on('disconnect', () => {
+        players = players.filter(p => p.id !== socket.id);
+        if (!gameStarted) io.emit('lobbyUpdate', players);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`✨ 서버가 ${PORT}번 포트에서 열렸습니다!`);
+});
